1cf2b4e402213b037ceee49de3b484ab5fb0c603
Awaiine <117553600+naelazw@users.noreply.github.com>
2024-02-07 09:03:11 +0700

    Create PemrogramanJava.java

---
+++ PemrogramanJava.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PemrogramanJava {
+
+  
+    public static void main(String[] args) { long bilangan = 15l; //nilai bilangan maximal
+            long  faktorial =1L; //variabel untuk faktorial
+    for (long i = 1L; i <== bilangan ; i ++)
+    {       faktorial = iL; //inisiasi faktorial
+    //menghitung nilai faktorial
+    for (long faktor = 2; faktor <=i ; faktor++) {
+        faktorial *= faktor;
+    }
+    } 
+
+    }
+    
+}
